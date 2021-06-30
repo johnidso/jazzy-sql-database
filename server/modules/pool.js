9e031b5c80edb1aac7acba538be2f795a02dd165
John Idso <john.d.idso@gmail.com>
2021-06-29 19:12:58 -0500

    setting up router, pool module, server.js to handle

---
+++ server/modules/pool.js
@@ -0,0 +1,19 @@
+const pg = require('pg');
+const Pool = pg.Pool;
+const pool = new Pool({
+    database: 'jazzy.sql',
+    host: 'localhost',
+    port: 5432,
+    max: 10,
+    idleTimeoutMillis: 30000
+});
+
+pool.on('connect', () => {
+    console.log('Postresql connected');
+});
+
+pool.on('error', error => {
+    console.log('Error with postgres pool', error);
+});
+
+module.exports = pool;
